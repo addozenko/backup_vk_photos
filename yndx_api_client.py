1a5e21ee9eb6608bdfc4a084b2d7c2da06804e8f
root <root@root>
2024-12-06 06:56:00 +0300

    Add def put_json_file

--- yndx_api_client.py
+++ yndx_api_client.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 
 from dotenv import load_dotenv
 from pprint import pprint
@@ -28,20 +29,37 @@ class YNDXAPIClient:
             'path' : path, 
         }
         return params
-    
-    def create_folder(self, folder_name):
+    def __create_json_file(self, file_name, photo_data):
+        with open(file_name, 'w') as f:
+            json.dump(photo_data, f)
+
+    def __get_url_for_upload(self, file_name):
+        params = self.__set_params(file_name)
+        response = requests.get(self.__build_url('resources/upload'), headers=self.__set_headers(), params=params)
+        print(response.json().get('href'))
+        return response.json().get('href')
+
+    def put_folder(self, folder_name):
         params = self.__set_params(folder_name)
-        requests.put(self.__build_url('resources'), headers=self.__set_headers(), params=params)
+        requests.put(self.__build_url('resources'), headers=self.__set_headers(), params=params)    
 
     def delete_folder(self, folder_name, permanently=False):
         params = self.__set_params(folder_name)
         params.update({'permanently' : permanently})
         requests.delete(self.__build_url('resources'), headers=self.__set_headers(), params=params)
 
-    def set_photo(self, filename, url):
-        params = self.__set_params(filename)
+    def post_photo(self, file_name, url):
+        params = self.__set_params(file_name)
         params.update({'url' : url})
         requests.post(self.__build_url('resources/upload'), headers=self.__set_headers(), params=params)
+
+    def put_json_file(self, name, photo_data):
+        file_name = f'{name}.json'
+        self.__create_json_file(file_name, photo_data)
+        with open(file_name, 'r') as f:
+            requests.put(self.__get_url_for_upload(file_name), files = {'file': f})
+        os.remove(f"./{file_name}")
+        
 
 if __name__ == '__main__':
     load_dotenv()
@@ -50,7 +68,10 @@ if __name__ == '__main__':
     user_id = os.getenv('USER_ID')
     yndx_conn = YNDXAPIClient(yndx_access_token)
     vk_conn = VKAPIClient(vk_access_token, user_id)
+    yndx_conn.create_folder('images')
     for photo_array in vk_conn.get_photos():
         photo_url = photo_array.pop('url')
         photo_name = photo_array['file_name']
-        yndx_conn.set_photo(photo_name, photo_url)
+        file_name = photo_array['file_name'].split('.')[0]
+        yndx_conn.put_json_file(file_name, photo_array)
+        yndx_conn.post_photo(photo_name, photo_url)
